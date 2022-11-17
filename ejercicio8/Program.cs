9b6383d02fe501cac5056903b9461028ef97dff1
TONYMOR1208 <106847101+TONYMOR1208@users.noreply.github.com>
2022-11-17 13:02:52 -0500

    Add files via upload

---
+++ ejercicio8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ejercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i=0; i <= 3000; i += 3) 
+
+                Console.WriteLine(" i : " + i);
+        }
+    }
+}
