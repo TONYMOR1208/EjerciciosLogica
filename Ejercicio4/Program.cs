9c42eb432dc442d6e0303b841d0e026be8725e05
TONYMOR1208 <106847101+TONYMOR1208@users.noreply.github.com>
2022-11-17 12:55:40 -0500

    Add files via upload

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num =0;
+            int suma =0;
+
+            do
+            {
+                Console.WriteLine("dijite un numero: ");
+                num = int.Parse(Console.ReadLine());
+                suma = suma + num;
+            }while (num != 0);
+            Console.WriteLine("La suma es : "+suma);
+            Console.ReadKey();
+
+
+
+
+        }
+
+    }
+}
---
+++ ejercicio5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir un programa que lea exactamente 8 números y luego escriba la suma de todos ellos
+
+
+            int numeros;
+            int  suma  = 0;
+            int i;
+
+            Console.WriteLine(" Introducir 8 numeros: ");
+            for (i = 0; i < 8; i++)
+            {
+                Console.WriteLine("Introduce un numero entero");
+                numeros = int.Parse(Console.ReadLine());
+                suma = suma + numeros;
+            }
+                    Console.WriteLine(" La suma de los numeros es de :  " + suma);
+            Console.ReadKey();  
+
+            
+    }
+    }
+}
