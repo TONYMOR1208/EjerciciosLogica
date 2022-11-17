a5112d970c9d879a7c26fc86aeee4b0e3bc0574a
TONYMOR1208 <106847101+TONYMOR1208@users.noreply.github.com>
2022-11-17 13:03:39 -0500

    Add files via upload

---
+++ ejercicio10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ejercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programa que lea un número entero por teclado y que calcule su factorial.
+
+
+
+
+                int x;
+                double fact = 1;
+
+                Console.WriteLine("Escriba el número: ");
+                x= int.Parse(Console.ReadLine());
+                while (x > 1) fact *= x--;
+                Console.WriteLine("Factorial = : " + fact);
+
+            }
+
+        }
+    }
+
