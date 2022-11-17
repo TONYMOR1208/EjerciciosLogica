865954ce14444992ee6a85de65dcd4b1c7755971
TONYMOR1208 <106847101+TONYMOR1208@users.noreply.github.com>
2022-11-17 13:01:49 -0500

    Add files via upload

---
+++ ejercicio9/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace ejercicio9
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numeros = 1;
+            double suma = 0;
+
+            double c = 0;
+            do
+            {
+
+                Console.WriteLine("Introduce un numero entero");
+                numeros = double.Parse(Console.ReadLine());
+                if (numeros !=0)
+                {
+                    suma = suma + numeros;
+
+                    c = c + 1;
+                }
+               
+            } while (numeros != 0);
+            Console.WriteLine(" La suma de los numeros es de :  " + suma);
+
+            Console.WriteLine("EL PROMEDIO DE LOS NUMEROS INGRESADOS ES: " + suma / c);
+          
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
