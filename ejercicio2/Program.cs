711b22db28bde7e2b7c7ddc3bee3da40fbd9081a
TONYMOR1208 <106847101+TONYMOR1208@users.noreply.github.com>
2022-11-17 12:53:03 -0500

    Add files via upload

---
+++ ejercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int VARIABLE;
+
+            Console.WriteLine("Dijite un numero");
+
+            VARIABLE = int.Parse(Console.ReadLine());
+            
+            if (VARIABLE % 2 == 0)
+            {
+
+                Console.WriteLine("El numero "+ VARIABLE+ " es par");
+            }
+           else
+                Console.WriteLine("El numero "+ VARIABLE+ " es impar");
+         
+       
+        }
+    }
+}
---
+++ ejercicio3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ejercicio3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int numero_1, numero_2, numero_3;
+
+			Console.WriteLine("Dijite el primer numero");
+			numero_1 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Dijite el segundo numero");
+			numero_2 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Dijite el tercer numero");
+			numero_3 = int.Parse(Console.ReadLine());
+			if (numero_1 > numero_2 & numero_1 > numero_3)
+			{
+				Console.WriteLine("El primer numero es el mayor");
+			}
+			else
+
+		if (numero_2 > numero_1 && numero_2 > numero_3)
+			{
+				Console.WriteLine("El segundo numero es el mayor");
+			}
+			else
+
+			if (numero_3 > numero_1 && numero_3 > numero_2)
+			{
+				Console.WriteLine("El tercer numero es el mayor");
+
+				{ 
+				}
+			}
+
+		
+
+		}
+	}
+}
